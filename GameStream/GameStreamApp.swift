3eff3ae1ffa6b31f8bf4a84a94c8df4176f2eeb8
Edwin Yovany <yovany.dev@gmail.com>
2024-03-19 21:29:45 -0500

    Initial Commit

---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Edwin Yovany on 19/03/24.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
